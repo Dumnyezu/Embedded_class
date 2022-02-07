41397eeb71fe9b0422ec461216bfaa9e29610708
Dumnyezu <metal.csibesz@gmail.com>
2022-02-07 13:49:25 +0100

    process implemented

--- FirebaseComm.py
+++ FirebaseComm.py
@@ -1,4 +1,5 @@
 import pyrebase
+import multiprocessing as mp
 import RPi.GPIO as GPIO
 import logging
 import time
@@ -41,28 +42,32 @@ class FirebaseCom():
         self.red_pwm.start(0)  # start the program with 0% duty cycle (red LED will be OFF).
         self.blue_pwm.start(0)  # start the program with 0% duty cycle (blue LED will be OFF).
         self.green_pwm.start(0)
+        self.LEDlight_proc = mp.Process(target=self.LEDlooping, args=(self.redValue, self.greenValue,
+                                                                      self.blueValue), daemon=False)
 
     def setLEDs(self):
 
-        self.redValue = self.db.child("LEDctrl").child("RED").get().val()
-        self.greenValue = self.db.child("LEDctrl").child("GREEN").get().val()
-        self.blueValue = self.db.child("LEDctrl").child("BLUE").get().val()
-        self.FreqValue = self.db.child("LEDctrl").child("freq").get().val()
-        if (int(self.FreqValue) == 0):
-            self.FreqValue = "10"
-        elif (int(self.FreqValue) > 0):            
-            self.red_pwm.ChangeFrequency(int(self.FreqValue))
-            self.green_pwm.ChangeFrequency(int(self.FreqValue))
-            self.blue_pwm.ChangeFrequency(int(self.FreqValue))
-            self.db.child("LEDctrl").child("ack").set("0")
-            self.ack_bool = False
-            self._log.debug("Got value of redValue %s", self.redValue)
-            #self._log.debug("Got value of Sample %s", Sample.val())
-            while(self.ack_bool == False):
-                self.red_pwm.ChangeDutyCycle(int(self.redValue))
-                self.green_pwm.ChangeDutyCycle(int(self.greenValue))
-                self.blue_pwm.ChangeDutyCycle(int(self.blueValue))
-
+        while (self.db.child("LEDctrl").child("ack").get().val() == "1"):
+            self.redValue = self.db.child("LEDctrl").child("RED").get().val()
+            self.greenValue = self.db.child("LEDctrl").child("GREEN").get().val()
+            self.blueValue = self.db.child("LEDctrl").child("BLUE").get().val()
+            self.FreqValue = self.db.child("LEDctrl").child("freq").get().val()
+            if (int(self.FreqValue) == 0):
+                self.FreqValue = "10"
+            elif (int(self.FreqValue) > 0):
+                self.red_pwm.ChangeFrequency(int(self.FreqValue))
+                self.green_pwm.ChangeFrequency(int(self.FreqValue))
+                self.blue_pwm.ChangeFrequency(int(self.FreqValue))
+                self.db.child("LEDctrl").child("ack").set("0")
+                self._log.debug("Got value of redValue %s", self.redValue)
+            if self.LEDlight_proc.is_alive() == True:
+                print("capture_proc is alive")
+                self.LEDlight_proc.kill()
+                print("capture_proc is killed")
+            else:
+                self.LEDlight_proc = mp.Process(target=self.LEDlooping, args=(self.redValue,
+                                self.greenValue, self.blueValue), daemon=False)
+                self.LEDlight_proc.start()
 
     def getData(self):
         self.ack = self.db.child("LEDctrl").child("ack").get().val()
@@ -71,7 +76,13 @@ class FirebaseCom():
         if (self.powerstate == "1" and self.ack == "1"):
             self.ack_bool = True
             self.setLEDs()
-    
+
+    def LEDlooping(self, red, green, blue):
+        while 1:
+            self.red_pwm.ChangeDutyCycle(int(red))
+            self.green_pwm.ChangeDutyCycle(int(green))
+            self.blue_pwm.ChangeDutyCycle(int(blue))
+
     def LEDtest(self):
         
         while True:
